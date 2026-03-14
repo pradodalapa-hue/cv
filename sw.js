fb78717d0893c2ff8229ddf62183840ebb6fb985
pradodalapa-hue <pradodalapa@gmail.com>
2026-03-14 06:17:40 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,1 @@
 
-const CACHE_NAME = 'jdp-industrial-v1';
-self.addEventListener('install', (e) => { console.log('SISTEMA JDP ATIVO'); });
-self.addEventListener('fetch', (event) => {
-    if (location.hostname.includes('github.io') || location.href.includes('.zip')) {
-        console.error('ACESSO NÃO AUTORIZADO');
-    }
-    event.respondWith(fetch(event.request));
-});
